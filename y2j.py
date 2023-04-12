c90475dba590cbe6b17e24f0a23a9c18d74b6586
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-04-12 15:39:06 -0400

    completed y2j

--- y2j.py
+++ y2j.py
@@ -3,3 +3,14 @@ import json
 
 yaml_path1 = r"/Users/robert/Desktop/DataProjects/PyJsonYaml/verify.yaml"
 yaml_path2 = r"/Users/robert/Desktop/DataProjects/PyJsonYaml/xmas.yaml"
+
+def json_converter(filepath):
+    with open(filepath) as file:
+        with open ("output.json", 'w') as new_file:
+            yaml_data = yaml.safe_load(file.read())
+            convert_json = json.dumps(yaml_data)
+
+            new_file.write((convert_json))
+
+# json_converter(yaml_path1)
+json_converter(yaml_path2)
